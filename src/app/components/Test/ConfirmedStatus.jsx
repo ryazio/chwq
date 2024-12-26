96cbf33d3eb25728ea8dcab9dbb5be52f91c90e0
Dheeraj Arora <dheeraj.arora@ryaz.io>
2024-12-26 12:56:57 +0530

    fix: confirmed and loading status component

--- src/app/components/Test/ConfirmedStatus.jsx
+++ src/app/components/Test/ConfirmedStatus.jsx
@@ -7,7 +7,7 @@ import UploadIcon from "@/lib/icon/UploadIcon";
 const ConfirmedStatus = ({ status }) => {
   return (
     <div className="flex flex-col">
-      <div className="flex flex-col items-center justify-center gap-4 bg-[#0000004D] h-[200] w-[200] rounded-[15]">
+      <div className="flex flex-col items-center justify-center gap-4 bg-[#0000004D] h-[200px] w-[200px] rounded-[15]">
         <UploadIcon />
         <span className="font-[390] text-[14px] ">Code Submitted!</span>
       </div>
--- src/app/components/Test/LoadingStatus.jsx
+++ src/app/components/Test/LoadingStatus.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 const LoadingStatus = ({ status }) => {
   return (
     <div className="flex flex-col">
-      <div className="flex flex-col items-center justify-center gap-4 bg-[#0000004D] h-[200] w-[200] rounded-[15]">
+      <div className="flex flex-col items-center justify-center gap-4 bg-[#0000004D] h-[200px] w-[200px] rounded-[15]">
         <LoadingIcon />
         <span className="font-[390] text-[14px] ">Checking your code!</span>
       </div>
